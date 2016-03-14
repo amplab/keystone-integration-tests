80200de299a62f9ffa109fd3fb37dac4299816f0
Evan Sparks <sparks@cs.berkeley.edu>
2016-03-14 11:40:13 -0700

    Programmatically reflecting pipeline timing and accuracy.

--- src/main/scala/TestRunner.scala
+++ src/main/scala/TestRunner.scala
@@ -6,26 +6,16 @@ object TestRunner extends Logging {
   def main(args: Array[String]) = {
     val testName = if (args.length < 1) "all" else args(0)
 
-    val sc = new SparkContext(new SparkConf().setAppName("TestRunner: " + testName))
-
-    val tests = Map(
-      "cifar" -> cifar,
-      "voc" -> voc,
-      "newsgroups" -> newsgroups
-    )
+    val sc = new SparkContext(new SparkConf().setAppName(s"TestRunner($testName)"))
 
     val runs = testName match {
       case "all" => tests.keys
       case _ => Seq(testName)
     }
 
-    runs.foreach(t => {
-      logInfo(s"Running test $t")
-      val start = System.currentTimeMillis()
-      tests(t)(sc)
-      val diff = System.currentTimeMillis() - start
-      logInfo(s"Finished test $t in $diff")
-    })
+    val results = runs.map(t => (t, tests(t)(sc)))
+
+    results.foreach(s => logInfo(s.toString))
 
     sc.stop()
 
