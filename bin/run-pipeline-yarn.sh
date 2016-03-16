4c02ba320a20db3dbbc4efbeb96d73fb1c5a9f5c
etrain <sparks@cs.berkeley.edu>
2016-03-15 22:57:25 -0700

    Making library paths consistent.

--- bin/run-pipeline-yarn.sh
+++ bin/run-pipeline-yarn.sh
@@ -27,6 +27,7 @@ JARFILE="$FWDIR/target/scala-2.10/keystone-app-assembly.jar"
 # TODO: Figure out a way to pass in either a conf file / flags to spark-submit
 KEYSTONE_MEM=${KEYSTONE_MEM:-1g}
 export KEYSTONE_MEM
+LD_LIBRARY_PATH=/opt/amp/gcc/lib64:/opt/amp/openblas/lib:$LD_LIBRARY_PATH\
 
 # Set some commonly used config flags on the cluster
 spark-submit \
@@ -35,8 +36,8 @@ spark-submit \
   --num-executors 10 \
   --executor-cores 10 \
   --driver-class-path $JARFILE:$HOME/hadoop/conf \
-  --driver-library-path /home/eecs/shivaram/openblas-install/lib:$FWDIR/../lib:$KEYSTONE_DIR/lib \
-  --conf spark.executor.extraLibraryPath=/home/eecs/shivaram/openblas-install/lib:$FWDIR/../lib:$KEYSTONE_DIR/lib \
+  --driver-library-path /opt/amp/openblas/lib:$FWDIR/../lib:$KEYSTONE_DIR/lib \
+  --conf spark.executor.extraLibraryPath=/opt/amp/openblas/lib:$FWDIR/../lib:$KEYSTONE_DIR/lib \
   --conf spark.executor.extraClassPath=$JARFILE:$HOME/hadoop/conf \
   --conf spark.serializer=org.apache.spark.serializer.JavaSerializer \
   --conf spark.executorEnv.LD_LIBRARY_PATH=/opt/amp/gcc/lib64:/opt/amp/openblas/lib:$LD_LIBRARY_PATH\
